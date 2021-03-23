541b0d5110c3fe44f1adda0fe1ead46f0914fcec
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-23 23:58:52 +0100

    added formulaeMethod

---
+++ Dynamic Programming/Python Algorithms/Codility/Classic Algorithms/formularsMethod.py
@@ -0,0 +1,12 @@
+#Using Formulae
+
+# Formular Method @ constant time for calculating the sum of n numbers.
+#Model solution for sum of n numbers => Time Complexity O(1)
+def model_solution(n):
+    result = n * (n + 1) // 2
+    return result
+
+# def fib(n):
+#     return (((1 + 5^(1/2))// 2)^n - ((1 - 5^(1/2))//2 )^n)// 5^(1/2)
+
+# print(fib(5)) 
