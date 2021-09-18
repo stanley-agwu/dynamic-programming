38b48097c921fadd29a4b3c58965f65e70ee676e
Stanley Agwu <stanley.agwu@sollers.eu>
2021-09-18 09:30:41 +0200

    Added Solution to flag Codility Problem

--- Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/flag.js
+++ Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/flag.js
@@ -1,58 +1,42 @@
 //Codility Flags Problem
 
-//Average Solution **53%**
+//Average solution **100%***
 
 //Find the maximum number of flags that can be set on mountain peaks.
 
-const peaks = (A) => {
-    const N = A.length
-    // const numsArray = Array(N + 1).fill(0)
+const flag = (A) => {
+    const N = A.length;
+    const peakArr = []
 
-    const peakPOSArray = []
-    for (let i = 1; i < N - 1; i += 1){
-        if (A[i] > A[i - 1] && A[i] > A[i + 1]){
-            peakPOSArray.push(i)
-            // numsArray[i] = "P"
+    for (let i = 1; i < N - 1 ; i += 1){
+        if (A[i] > A[i - 1] && A[i] > A[ i + 1]) {
+            peakArr.push(i)
         }
+        
     }
-    const numberOfPeaks = peakPOSArray.length
-
-    const results = [numberOfPeaks, peakPOSArray]
-    return results
-}
-
-const flag = (A) =>{
-    const N = A.length
-
-    if (N < 3) return 0
-
-    const [maxFlags, peakPOS] = peaks(A)
-
-    //peakPOS => [1, 3, 5, 10]
-
-    if (maxFlags === 1) return 1
-
+    const M = peakArr.length
+    
     let maxCount = 0
-    let numFlags = maxFlags
-    while (numFlags > 1){
-        let count = 1
-        let i = j = 0
-        while (i < peakPOS.length){
-            j = i + 1
-            while (j <= peakPOS.length && count < numFlags){
-                if ((peakPOS[i] + numFlags ) <= peakPOS[j]){
-                    count += 1
-                    maxCount = Math.max(maxCount, count)
-                    i = j - 1
-                    break 
-                }
-                j += 1
+    if (M < 3) return M
+    let p = 3
+    const limit = Math.floor(Math.sqrt(N) + 2)
+    while (p < limit){
+        let i = 1
+        let left = 0
+        let count = 0
+        while (i < M && maxCount < p){ 
+            if (Math.abs(peakArr[i] - peakArr[left]) >= p){
+                count += 1
+                if (left === 0) count += 1
+                maxCount = Math.max(maxCount, count)
+                left = i
             }
             i += 1
         }
-        numFlags -= 1
+        p += 1
     }
     return maxCount
 }
 
-console.log(flag([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]))
+console.log(flag([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]));
+console.log(flag([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2, 4, 1, 6, 3, 2]));
