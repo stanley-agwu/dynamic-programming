9b9bd66f9c305abb506b6ac5d1225f87ae4df75c
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-07-20 07:36:16 +0200

    reverse integer

---
+++ Dynamic Programming/JavaScript Algorithms/BootCamp/Problem Algorithms/reverseInteger.js
@@ -0,0 +1,14 @@
+//Given an integer, return an integer that is the reverse ordering
+//of numbers.
+
+
+const reverseInt = (n) => {
+    const reversed = n.toString().split('').reverse().join('');
+  
+    return parseInt(reversed) * Math.sign(n);
+  }
+  console.log(reverseInt(15));//51
+  console.log(reverseInt(981));//189
+  console.log(reverseInt(500));//5
+  console.log(reverseInt(-90));//-9
+  console.log(reverseInt(-15));//-51
