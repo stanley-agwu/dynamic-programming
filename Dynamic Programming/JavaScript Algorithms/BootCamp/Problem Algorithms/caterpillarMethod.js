305df080e07715bf479f956e140579a461514d72
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-07-20 06:12:55 +0200

    caterpillar method

---
+++ Dynamic Programming/JavaScript Algorithms/BootCamp/Problem Algorithms/caterpillarMethod.js
@@ -0,0 +1,33 @@
+//Find the longest sub-array with sum S
+
+
+//Using the Caterpillar Method (with pointers)
+
+const longestSubArray = (A, S) => {
+    const N = A.length 
+    let result = value = sum = left = right = 0
+    let pointers = []
+    let pointA = 0;
+    let pointB =  0;
+
+    while (left < N){
+        while (right < N && sum + A[right] <= S){
+            pointA = left
+            sum += A[right]
+            if(sum === S) pointB = right
+            right += 1
+        }
+        sum -= A[left]
+
+        value = pointB - pointA
+        if (value > result) pointers = [pointA, pointB]
+        result = Math.max(value, result)
+        
+        left += 1
+    }
+    return pointers
+
+}
+
+console.log(longestSubArray([ 0, 0, 0, 0, 7, 0, 0, 0, 8, 1, 2, 3, 4, 5, 0, 0, 0, 6, 7, 8, 9, 10], 15));
+console.log(longestSubArray([ 7, 8, 1, 2, 3, 4, 5, 0, 0, 0, 6, 7, 8, 9, 10], 15));
