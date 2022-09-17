d40df912f38614bed2fc33c09d847eb13b986cb5
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-09-17 09:38:59 +0200

    maxCounter - updated code to cover edge cases for perfect score

--- Dynamic Programming/Python Algorithms/Codility/Problem Algorithms/Respectable/maxCounter.py
+++ Dynamic Programming/Python Algorithms/Codility/Problem Algorithms/Respectable/maxCounter.py
@@ -1,26 +1,28 @@
 #Codility Max Counter problem
 
-#CORRECT BUT POOR PERFORMANCE 88%
+#Golden solution ** 100% ** O(N + M)
 
 def maxCounter(N, A):
     counter = [0] * N
     
     max_value = 0
     current_value = 0
-    for num in A:
-        if (num <= N):
-            counter[num - 1] += 1
-            current_value = counter[num - 1]
-            if(max_value < current_value): 
-                max_value = current_value
-
-
-        if (num == N + 1):
-            counter = [max_value] * N
-
+    last_maxValue = 0
+    for i in range(len(A)):
+        if (A[i] < N + 1):
+            if (counter[A[i] - 1] < last_maxValue):
+               counter[A[i] - 1] = last_maxValue + 1
+            else:
+                counter[A[i] - 1] += 1
+            current_value = counter[A[i] - 1]
+            max_value = max(max_value, current_value)
+        else:
+            last_maxValue = max_value
+    
+    for i in range(len(counter)):
+        if (counter[i] < last_maxValue):
+            counter[i] = last_maxValue
     return counter
 
-print(maxCounter(5, [3, 4, 4, 6, 1, 4, 4]))
+print(maxCounter(5, [3, 4, 4, 6, 1, 4, 4])) # [3, 2, 2, 4, 2]
 
-
-            
