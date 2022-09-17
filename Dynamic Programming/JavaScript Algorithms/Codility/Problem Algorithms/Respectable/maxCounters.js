6d724121ba05582eed002e78d17f8e7e3117b8fc
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-09-17 08:49:03 +0200

    Updated maxCounter - resolved edge cases for perfect score

--- Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/maxCounters.js
+++ Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/maxCounters.js
@@ -4,28 +4,36 @@
 //increase counter by 1; set value of all counters to current maximum.
 
 
-//Good Solution **88%**
+//Golden Solution **100%**
 
 const maxCounters = (N, A) =>{
-    let counter = new Array(N).fill(0)
+    let counters = Array(N).fill(0);
+    const C = counters.length;
+    let maxCount = 0;
+    let minCount = 0;
 
-    let maxCount = 0
-    let m = 0
-
-    for (let val of A){
-        if (val < N + 1){
-            counter[val - 1] += 1
-            maxCount = Math.max(maxCount, counter[val - 1])
-            m = 1
-        } else if (val > N && m === 1){
-            counter = Array(N).fill(maxCount) 
-            //used m variable to prevent this O(n) operation from running when max counter is called consecutively
-            m = 2
+    for(let i of A) {
+        if (i < N + 1) {
+            if (counters[i - 1] < minCount) {
+                counters[i - 1] = 1 + minCount;
+            } else {
+                counters[i - 1] = counters[i - 1] + 1;
+            }
+            maxCount = Math.max(maxCount, counters[i - 1])
+        } else {
+            minCount = maxCount;
         }
     }
-    return counter
+
+    for (let i = 0; i < C; i++) {
+        if (counters[i] < minCount) {
+            counters[i] = minCount
+        }
+    }
+    return counters;
 }
 
-console.log(maxCounters(5, [3, 4, 4, 6, 1, 4, 4]))
-console.log(maxCounters(5, [2, 3]))
-console.log(maxCounters(5, [6, 4, 4, 4]))
+console.log(maxCounters(5, [3, 4, 4, 6, 1, 4, 4])) // [ 3, 2, 2, 4, 2 ]
+console.log(maxCounters(5, [2, 3])) // [ 0, 1, 1, 0, 0 ]
+console.log(maxCounters(5, [6, 4, 4, 4])) // [ 0, 0, 0, 3, 0 ]
+console.log(maxCounters(5, [3, 2, 2, 2, 4, 4, 4, 6, 1, 4, 4, 6, 6, 1, 1, 1, 1, 1, 1, 6, 1])) // [ 12, 11, 11, 11, 11 ]
