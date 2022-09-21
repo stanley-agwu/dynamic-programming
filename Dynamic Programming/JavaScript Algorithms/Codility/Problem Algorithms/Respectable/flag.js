0969e67e5976676df8f8ddc9f02870da2f23f3b3
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-09-21 10:18:16 +0200

    Updated  - Codility Flag problem to perfect score considering all edge cases

--- Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/flag.js
+++ Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/flag.js
@@ -1,42 +1,59 @@
 //Codility Flags Problem
 
-//Average solution **100%***
-
 //Find the maximum number of flags that can be set on mountain peaks.
 
-const flag = (A) => {
+// Good solution ** 100%** O(N)
+const flags = (A) => {
     const N = A.length;
-    const peakArr = []
 
-    for (let i = 1; i < N - 1 ; i += 1){
-        if (A[i] > A[i - 1] && A[i] > A[ i + 1]) {
-            peakArr.push(i)
-        }
-        
-    }
-    const M = peakArr.length
-    
-    let maxCount = 0
-    if (M < 3) return M
-    let p = 3
-    const limit = Math.floor(Math.sqrt(N) + 2)
-    while (p < limit){
-        let i = 1
-        let left = 0
-        let count = 0
-        while (i < M && maxCount < p){ 
-            if (Math.abs(peakArr[i] - peakArr[left]) >= p){
-                count += 1
-                if (left === 0) count += 1
-                maxCount = Math.max(maxCount, count)
-                left = i
+    const getPeaks = (Arr) => {
+        const N = Arr.length;
+        let P = [];
+        for (let i = 1; i < N - 1; i++){
+            if (Arr[i] > Arr[i - 1] && Arr[i] > Arr[i + 1]) {
+                P.push(i)
             }
-            i += 1
         }
-        p += 1
+        return P;
     }
-    return maxCount
+    const setFlags = (peaksArr, flagsNeeded) => {
+        let setFlags = 0;
+        let lastIndex = -flagsNeeded;
+        for (const idx of peaksArr) {
+            if (idx >= lastIndex + flagsNeeded) {
+                setFlags += 1;
+                if (setFlags === flagsNeeded) {
+                    return setFlags;
+                }
+                lastIndex = idx;
+            }
+        }
+        return setFlags;
+    }
+    if (N < 3) return 0;
+
+    const Peaks = getPeaks(A);
+    if (Peaks.length < 2) return Peaks.length;
+
+    const maxPeaks = Math.floor(Math.sqrt(N) + 1);
+    let results = 0;
+    for(let i = 1; i <= maxPeaks; i++) {
+        const flags = setFlags(Peaks, i);
+        results = Math.max(results, flags);
+    }
+    return results;
 }
 
-console.log(flag([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]));
-console.log(flag([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2, 4, 1, 6, 3, 2]));
+
+console.log(flags([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])) // 3
+console.log(flags([1, 3, 2])) // 1
+console.log(flags([0, 1, 0, 1, 0, 1, 0, 1, 0, 1])) // 2
+console.log(flags([1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1])) // 1
+console.log(flags([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])) // 0
+console.log(flags([0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 5, 3, 1, 3, 0, 6, 1])) // 2
+// Peaks: [ 1, 3, 5, 10 ]
+// Peaks: [ 1 ]
+// Peaks: [ 1, 3, 5, 7 ]
+// Peaks: [ 8 ]
+// Peaks: []
+// Peaks: [ 1, 7, 10, 13, 15 ]
