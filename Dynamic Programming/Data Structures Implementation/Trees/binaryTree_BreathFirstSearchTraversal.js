a76df5a01474df1673dbec7793a15a29b09c6350
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-09-21 10:14:36 +0200

    Updated - breathFirstSearchTraversal

--- Dynamic Programming/Data Structures Implementation/Trees/binaryTree_BreathFirstSearchTraversal.js
+++ Dynamic Programming/Data Structures Implementation/Trees/binaryTree_BreathFirstSearchTraversal.js
@@ -39,7 +39,7 @@ const breathFirstSearchTraversal = (root) => {
     const queue = [ root ]
     while (queue.length > 0){
         const curr = queue.shift()
-        console.log(curr.val)
+        // console.log(curr.val)
         if (curr.left !== null){
             queue.push(curr.left)
         }
@@ -52,7 +52,7 @@ const breathFirstSearchTraversal = (root) => {
 breathFirstSearchTraversal(A)
 
 
-//Write a function, bsf(root, target), that takes in the root of a tree and its target value 
+//Write a function, bfs(root, target), that takes in the root of a tree and its target value 
 //as arguements. The function should return a boolean indicating whether or not the binary tree
 //contains the target value.
 
@@ -71,15 +71,15 @@ breathFirstSearchTraversal(A)
 // const E = new Node ("E")
 // const F = new Node ("F")
 
-// A.left = B 
-// A.right = C
-// B.left = D
-// B.right = E
-// C.right = F
+A.left = B 
+A.right = C
+B.left = D
+B.right = E
+C.right = F
 
-const bsf = (root, target) => {
+const bfs = (root, target) => {
     const queue = [ root ]
-    while (queue.length > 0){
+    while (queue.length){
         const curr = queue.shift()
         if (curr.val === target){
             return true
@@ -94,6 +94,50 @@ const bsf = (root, target) => {
     return false
 }
 
-console.log(bsf (A, "E")) // true
-console.log(bsf (A, "Z")) // false
-console.log(bsf (A, "S")) // false
+console.log(bfs (A, "E")) // true
+console.log(bfs (A, "Z")) // false
+console.log(bfs (A, "S")) // false
+
+
+//Write a function, sumTree(root), that takes in the root of a tree and its target value 
+//as arguements. The function should return the total sum of all values in the tree.
+//You can assume that the tree only contains number values.
+
+class Node{
+    constructor(val){
+        this.val = val 
+        this.left = null 
+        this.right = null
+    }
+}
+
+const a = new Node (3)
+const b = new Node (2)
+const c = new Node (7)
+const d = new Node (4)
+const e = new Node (-2)
+const f = new Node (5)
+
+a.left = b; 
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const sumTree = (root) => {
+    const queue = [ root ]
+    let count = 0;
+    while (queue.length){
+        const curr = queue.shift()
+        count += curr.target;
+        if (curr.left !== null){
+            queue.push(curr.left)
+        }
+        if (curr.right !== null){
+            queue.push(curr.right)
+        }
+    }
+    return count;
+}
+
+console.log(sumTree(a)) // 19
