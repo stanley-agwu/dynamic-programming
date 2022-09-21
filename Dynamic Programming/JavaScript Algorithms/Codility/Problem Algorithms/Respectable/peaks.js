e01608b563e84c369326e1e67bde9cbfa597ab80
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-09-21 10:19:39 +0200

    Updated  - Codility Peaks problem to perfect score considering all edge cases

--- Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/peaks.js
+++ Dynamic Programming/JavaScript Algorithms/Codility/Problem Algorithms/Respectable/peaks.js
@@ -1,49 +1,61 @@
 //Codility Peaks Problem
 
-//Average Solution **54%**
-
 //Divide an array into the maximum number of same-sized blocks, 
 //each of which should contain an index P such that A[P - 1] 
-//< A[P] > A[P + 1].
+//< A[P] && A[P] > A[P + 1].
+
+// Golden solution **100%** O(N log(log N))
 
 const peaks = (A) => {
-    const N = A.length 
+    const N = A.length;
 
-    const peaksArray = []
-
-    if (N < 3) return 0
-
-    for (let i = 1; i < N - 1; i += 1){
-        if (A[i] > A[i - 1] && A[i] > A[i + 1]){
-            peaksArray.push(i)
-        }
-    }
-
-    const nrOfPeaks = peaksArray.length 
-    if (nrOfPeaks === 0) return 0
-    if (nrOfPeaks < 3 && N % nrOfPeaks === 0) return nrOfPeaks
-
-    let restNrOfPeaks = nrOfPeaks
-    if (restNrOfPeaks >= 3){
-        if (N % restNrOfPeaks === 0){
-            let numr = N/restNrOfPeaks 
-            let i = 1
-            while(i < peaksArray.length){
-                if ((peaksArray[i] - peaksArray[i - 1]) >= numr) {
-                    //console.log("Number of blocks: ", restNrOfPeaks)
-                    return restNrOfPeaks
-                }
-                i += 1
+    const getPeaks = (Arr) => {
+        const N = Arr.length;
+        const peaks = Array(N).fill(0);
+        for (let i = 1; i < N - 1; i++) {
+            if (Arr[i] > Arr[i - 1] && Arr[i] > Arr[i + 1]) {
+                peaks[i] = 1;
             }
-            restNrOfPeaks -= 1
-            //console.log("Number of blocks: ", restNrOfPeaks)
-            return restNrOfPeaks
-            
         }
-        return 1
+        return peaks;
     }
-}
+    const hasPeaks = (Arr, arrDiv) => {
+        const arrChunk = Arr.length / arrDiv;
+        let leftPointer = 0;
+        let rightPointer = arrChunk;
+        while (rightPointer < N + 1) {
+            let count = 0;
+            while (leftPointer < rightPointer) {
+                if (Arr[leftPointer] === 1) {
+                    count++;
+                }
+                leftPointer += 1;
+            }
+            if (count === 0) return false;
+            leftPointer = rightPointer;
+            rightPointer += arrChunk;
+        }
+        return true;
+    }
 
-console.log(peaks([1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])) // [3, 5, 10]
-console.log(peaks([3, 4, 3, 2, 5, 1, 2, 3, 3, 4, 6, 3, 3, 1, 8, 5]))// [1, 4, 10, 14]
-console.log(peaks([1, 2, 20, 4, 15, 4, 12, 2, 8, 4, 1, 2])) // [2, 4, 6, 8] => 3
+    const peaksArr = getPeaks(A);
+    const NrOfPeaks = peaksArr.reduce((sum, curr) => ( sum += curr), 0);
+    if (NrOfPeaks < 2) return NrOfPeaks;
+
+    let arrDiv = NrOfPeaks;
+    while (arrDiv > 1) {
+        if (N % arrDiv === 0) {
+            const hasPeaksValues = hasPeaks(peaksArr, arrDiv);
+            if (hasPeaksValues) return arrDiv;
+        } else {
+            arrDiv--;
+            continue;
+        };
+        arrDiv--;
+    }
+    return 1;
+};
+
+console.log(peaks([1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])); // 3
+console.log(peaks([1, 3, 2, 1])); // 1
+console.log(peaks([1, 3, 2, 1, 5, 3, 7, 5, 1, 7, 4, 9, 0, 2])); // 2
