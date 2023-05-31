ed040391e7f8b6e1aa4d1131b0c5616242d71284
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-05-31 23:58:50 +0200

    feat(easy): simple solution to flatten array

---
+++ Dynamic Programming/JavaScript Algorithms/BootCamp/Problem Algorithms/flattenArrayOfArrays.js
@@ -0,0 +1,8 @@
+// Flatten an array of arrays
+
+const flattened = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+].reduce((accumulator, currentValue) => accumulator.concat(currentValue), []);
+// flattened is [0, 1, 2, 3, 4, 5]
